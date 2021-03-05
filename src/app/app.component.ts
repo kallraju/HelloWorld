795e8d0c7fa0f8825e6ebd3b1ca7ed90a0c4f1a6
Teleparishkar.com <kallraju@gmail.com>
2021-03-05 11:58:05 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: "ns-app",
+    selector: "ns-app1",
     templateUrl: "app.component.html"
 })
 export class AppComponent { }
